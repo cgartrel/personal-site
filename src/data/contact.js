34c16ee89f63594efa0ba870fc5985e429060ea2
Mike Gartrell <cgartrel@gmail.com>
2024-02-14 01:33:28 +0100

    Added Google Scholar to contact info.

--- src/data/contact.js
+++ src/data/contact.js
@@ -4,6 +4,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faGoogleScholar } from '@fortawesome/free-brands-svg-icons/faGoogleScholar';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -38,6 +39,11 @@ const data = [
     link: 'https://twitter.com/mikegartrell',
     label: 'Twitter',
     icon: faTwitter,
+  },
+  {
+    link: 'https://scholar.google.com/citations?hl=en&user=NX6eiWYAAAAJ',
+    label: 'Google Scholar',
+    icon: faGoogleScholar,
   },
   {
     link: 'mailto:mike.gartrell@acm.org',
